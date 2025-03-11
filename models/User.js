6506cfc028c8b330bcd339bdb4c350384e851845
Abi-Malcom <abishek@malcom.ltd>
2025-03-11 18:38:43 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -6,28 +7,27 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true, // Prevent duplicate usernames
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true, // Prevent duplicate emails
-    },
     password: {
         type: String,
         required: true,
+        // Removed unique: true, as multiple users might have the same password (hashed).
     },
-    phone: {
+    mobile: {
+        type: String,
+        required: true,
+        unique: true, // Mobile numbers should be unique
+    },
+    language: {
+        type: String,
+        default: 'English',
+    },
+    address: {
         type: String,
         default: '',
-    },
-    language:{
-        type:String,
-        default:'English',
-    },
-    address:{
-        type:String,
-        default:'',
     }
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
